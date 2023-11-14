f6e7cdbe1cf92269e93b1202bbbc7f0576b64a39
Bruno Andreo <brunolino2026@gmail.com>
2023-11-13 22:01:29 -0300

    feat: added new functions

--- math.go
+++ math.go
@@ -9,3 +9,23 @@ func main() {
 func sum(a, b int) int {
 	return a + b
 }
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func times(a, b int) int {
+	return a * b
+}
+
+func sumX(a, b int) int {
+	return a + b + a
+}
+
+func sumX2(a, b int) int {
+	return a + b + a + b
+}
+
+func sumX3(a, b int) int {
+	return a + b + a + b + a
+}
--- math_test.go
+++ math_test.go
@@ -10,3 +10,48 @@ func TestSum(t *testing.T) {
 		t.Errorf("Result is invalid. Result: %d. Expected: %d", total, expected)
 	}
 }
+
+func TestSub(t *testing.T) {
+	expected := 0
+	result := sub(15, 15)
+
+	if result != expected {
+		t.Errorf("Result is invalid. Result: %d. Expected: %d", result, expected)
+	}
+}
+
+func TestTimes(t *testing.T) {
+	expected := 15
+	result := times(3, 5)
+
+	if result != expected {
+		t.Errorf("Result is invalid. Result: %d. Expected: %d", result, expected)
+	}
+}
+
+func TestSumX(t *testing.T) {
+	expected := 45
+	result := sumX(15, 15)
+
+	if result != expected {
+		t.Errorf("Result is invalid. Result: %d. Expected: %d", result, expected)
+	}
+}
+
+func TestSumX2(t *testing.T) {
+	expected := 60
+	result := sumX2(15, 15)
+
+	if result != expected {
+		t.Errorf("Result is invalid. Result: %d. Expected: %d", result, expected)
+	}
+}
+
+func TestSumX3(t *testing.T) {
+	expected := 75
+	result := sumX3(15, 15)
+
+	if result != expected {
+		t.Errorf("Result is invalid. Result: %d. Expected: %d", result, expected)
+	}
+}
